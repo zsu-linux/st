a77eb668ea3319292f53583efa0d3d157d73775d
zsu <zsu.work@gmail.com>
2022-04-10 18:58:33 +0800

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=25:type=Regular:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font:pixelsize=25:type=Regular:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
